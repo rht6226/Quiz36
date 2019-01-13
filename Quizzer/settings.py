b6584c171a2a6c1c0f18ec45b11de5edf91802ef
rht6226 <rohitraazanand567@gmail.com>
2019-01-13 22:46:34 +0530

    helaaaaa

--- Quizzer/settings.py
+++ Quizzer/settings.py
@@ -114,8 +114,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE =  'Asia/Kolkata'
+UTC=False
+# TIME_ZONE =  'Asia/Kolkata'
 USE_I18N = True
 
 USE_L10N = True
